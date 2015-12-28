3466ce668a41bc72d97af0700941f14f544c750a
Emily Block <emilybee3@gmail.com>
2015-12-28 12:10:12 -0800

    working solution- 5 MINS

--- pig_latin.py
+++ pig_latin.py
@@ -5,7 +5,30 @@
 
 # Rules
 
-# If the word begins with a consonant (not a, e, i, o, u), 
-# move the first letter to the end and add ‘ay’
-# If the word begins with a vowel, add ‘yay’ to the end
+# If the word begins with a consonant (not a, e, i, o, u),
+#move first letter to end and add "ay" to the end
+#if word begins with a vowel, add "yay" to the end
 
+###########################################################################################
+###########################################################################################
+
+#Example input:
+
+# "Hello" = "Ellohey"
+# "Android" = "Androidyay"
+
+def pig_latin(word):
+    #create a list of vowels for function to check the first letter
+    #of input word against
+    vowels = ["a", "e", "i", "o", "u"]
+
+    #first letter = vowel condition
+    if word[0] in vowels:
+        print word + "yay"
+
+    #first letter = consenent condition
+    else:
+        print word[1:] + word[0] + "ay"
+
+pig_latin("hello")
+pig_latin("android")
